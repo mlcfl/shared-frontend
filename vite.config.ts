67d8ca75c9627edbe3ecff16952a04186a701920
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2025-10-27 22:16:56 +0300

    build: create build as esm lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-	plugins: [vue(), vuetify({ autoImport: true })],
+	plugins: [
+		vue(),
+		vuetify({ autoImport: true }),
+		dts({
+			insertTypesEntry: true,
+			include: ["src"],
+		}),
+	],
 	build: {
 		lib: {
-			entry: "src/index.ts",
-			name: "SharedFrontend",
-			fileName: (format) => `index.${format}.js`,
+			entry: resolve(__dirname, "src/index.ts"),
+			fileName: "index",
 			formats: ["es"],
 		},
 		rollupOptions: {
-			external: ["vue", "vuetify"],
-			output: {
-				globals: {
-					vue: "Vue",
-					vuetify: "Vuetify",
-				},
-			},
+			external: ["vue", "vuetify", "vue-router", "pinia"],
 		},
+		sourcemap: true,
+		minify: "esbuild",
 	},
 });
